1f32dcee7195d758635db4c66614e54b39c8cce2
JulyKikuAkita <b92701105@gmail.com>
2021-10-03 23:59:11 -0700

    Initial Commit

---
+++ edutainment/edutainmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  edutainmentApp.swift
+//  edutainment
+//
+//  Created by Ifang Lee on 10/3/21.
+//
+
+import SwiftUI
+
+@main
+struct edutainmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
